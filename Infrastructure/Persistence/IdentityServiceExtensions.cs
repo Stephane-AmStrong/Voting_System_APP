9e21b757ba36a9edd95c1f56a358fc684d671152
Stéphane Adjakotan <stephane.adjakotan@gmail.com>
2022-08-28 22:46:18 +0100

    Update IdentityServiceExtensions.cs

--- Infrastructure/Persistence/IdentityServiceExtensions.cs
+++ Infrastructure/Persistence/IdentityServiceExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Application.Interfaces;
-using Domain.Entities;
+using Application.Interfaces;
 using Domain.Settings;
 using Persistence.Contexts;
 using Microsoft.AspNetCore.Identity;
@@ -7,12 +6,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text;
-
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Persistence.Repository;
-using System.Text.Json;
 using Newtonsoft.Json.Linq;
 using Application.Models;
 
@@ -22,13 +19,19 @@ namespace Persistence
     {
         public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<AppDbContext>(options =>
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseInMemoryDatabase("IdentityDb"));
+            }
+            else
+            {
+                services.AddDbContext<IdentityContext>(options =>
                 options.UseSqlServer(
-                    configuration.GetConnectionString("DefaultConnection"),
-                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
-
-
-            services.AddIdentity<Voter, IdentityRole>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+                    configuration.GetConnectionString("IdentityConnection"),
+                    b => b.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName)));
+            }
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
 
             services.AddTransient<IAccountRepository, AccountRepository>();
             services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
@@ -36,70 +39,67 @@ namespace Persistence
 
         public static void ConfigureJwtBearerService(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
             }).AddJwtBearer(o =>
-            {
-                o.RequireHttpsMetadata = false;
-                o.SaveToken = false;
-                o.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidateIssuerSigningKey = bool.Parse(configuration["JWTSettings:ValidateIssuerSigningKey"]),
-                    ValidateIssuer = bool.Parse(configuration["JWTSettings:ValidateIssuer"]),
-                    ValidateAudience = bool.Parse(configuration["JWTSettings:ValidateAudience"]),
-                    ValidateLifetime = bool.Parse(configuration["JWTSettings:ValidateLifetime"]),
-                    ClockSkew = TimeSpan.Zero,
-                    ValidIssuer = configuration["JWTSettings:Issuer"],
-                    ValidAudience = configuration["JWTSettings:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
-                };
-
-
-                o.Events = new JwtBearerEvents
-                {
-                    OnAuthenticationFailed = context =>
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
                     {
-                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        string message = "";
-                        message += FlattenException(StatusCodes.Status401Unauthorized, context.Exception);
-                        return Task.CompletedTask;
-                    },
-
-                    OnChallenge = context =>
+                        ValidateIssuerSigningKey = bool.Parse(configuration["JWTSettings:ValidateIssuerSigningKey"]),
+                        ValidateIssuer = bool.Parse(configuration["JWTSettings:ValidateIssuer"]),
+                        ValidateAudience = bool.Parse(configuration["JWTSettings:ValidateAudience"]),
+                        ValidateLifetime = bool.Parse(configuration["JWTSettings:ValidateLifetime"]),
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JWTSettings:Issuer"],
+                        ValidAudience = configuration["JWTSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
+                    };
+                    o.Events = new JwtBearerEvents()
                     {
-                        context.HandleResponse();
-                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        context.Response.ContentType = "application/json";
-
-                        var errorDetails = new JObject
+                        OnAuthenticationFailed = c =>
                         {
-                            //["error"] = context.Error,
-                            ["StatusCode"] = StatusCodes.Status401Unauthorized,
-                            ["Message"] = context.ErrorDescription
-                        };
+                            c.NoResult();
+                            c.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                            c.Response.ContentType = "text/plain";
+                            return c.Response.WriteAsync(c.Exception.ToString());
+                        },
 
-                        return context.Response.WriteAsync(errorDetails.ToString());
-                    },
-
-                    OnForbidden = context =>
-                    {
-                        context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                        context.Response.ContentType = "application/json";
-
-                        var errorDetails = new JObject
+                        OnChallenge = context =>
                         {
-                            ["StatusCode"] = StatusCodes.Status401Unauthorized,
-                            ["Message"] = "You are not Authorized"
-                        };
+                            context.HandleResponse();
+                            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                            context.Response.ContentType = "application/json";
 
-                        return context.Response.WriteAsync(JsonSerializer.Serialize(errorDetails));
-                    }
-                };
+                            var errorDetails = new JObject
+                            {
+                                ["StatusCode"] = StatusCodes.Status401Unauthorized,
+                                //["Message"] = context.ErrorDescription,
+                                ["Message"] = "Unauthorized"
+                            };
 
-            });
+                            return context.Response.WriteAsync(errorDetails.ToString());
+                        },
+
+                        OnForbidden = context =>
+                        {
+                            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                            context.Response.ContentType = "application/json";
+
+                            var errorDetails = new JObject
+                            {
+                                ["StatusCode"] = StatusCodes.Status403Forbidden,
+                                ["Message"] = "You are not authorized to access this resource"
+                            };
+
+                            return context.Response.WriteAsync(errorDetails.ToString());
+                        },
+                    };
+                });
         }
 
         public static string FlattenException(int statusCode, Exception exception)
@@ -120,7 +120,7 @@ namespace Persistence
                 ["Message"] = stringBuilder.ToString()
             };
 
-            return JsonSerializer.Serialize(errorDetails);
+            return System.Text.Json.JsonSerializer.Serialize(errorDetails);
         }
     }
 }
